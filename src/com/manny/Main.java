aca4904eed52e933ab37c7bff8ff9a57a6041527
Manny Aguilar <mannyaaguilar@yahoo.com>
2018-12-16 23:16:33 -0700

    first commit

---
+++ src/com/manny/Main.java
@@ -0,0 +1,20 @@
+package com.manny;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Java has 53 reserved words that are used for "keywords" that have a pre-defined meaning in the language
+        //IntelliJ will highlight keywords
+        //Expressions are building blocks of all Java programs
+        //Expressions are built with values, variables, operators and method calls
+
+        // a mile is equal to 1.609344 kilometers
+
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50){
+            System.out.println("This is an expression");
+        }
+    }
+}
